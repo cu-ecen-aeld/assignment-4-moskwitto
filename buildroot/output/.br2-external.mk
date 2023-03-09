8260531786fa819eca683e0ff2207f1c6ae53a54
JF0HE1 <datastructures@algorithms>
2023-03-09 02:29:24 +0000

    Still iterating

---
+++ buildroot/output/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/moskito/Desktop/Desktop/code/coursera/systems_prog/coursera/assignments-3-and-later-moskwitto/assignment-4-moskwitto/base_external
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += project_base
+BR2_EXTERNAL_DIRS += /home/moskito/Desktop/Desktop/code/coursera/systems_prog/coursera/assignments-3-and-later-moskwitto/assignment-4-moskwitto/base_external
+BR2_EXTERNAL_MKS += /home/moskito/Desktop/Desktop/code/coursera/systems_prog/coursera/assignments-3-and-later-moskwitto/assignment-4-moskwitto/base_external/external.mk
+export BR2_EXTERNAL_project_base_PATH = /home/moskito/Desktop/Desktop/code/coursera/systems_prog/coursera/assignments-3-and-later-moskwitto/assignment-4-moskwitto/base_external
+export BR2_EXTERNAL_project_base_DESC = project_base
